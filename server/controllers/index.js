483fdea788cd7d10db8a4ba6bdf25c8e25397016
arobcode2 <andrew-c-rob@live.com>
2018-07-07 11:19:43 -0500

    Implement POST and GET requests functionality for users

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -6,24 +6,20 @@ module.exports = {
       if (!req.body) {
         return res.status(400).send('Invalid GET Request');
       } else {
-        console.log("I am in the controller", req.body);
-        return res.status(200).send('you jade it');
-      }
-      models.messages.get().then(messages => {
-      console.log(messages);
-    });
+        models.messages.get().then(messages => {
+        var response = {results: messages};
+        return res.status(200).send(response);
+      });
+    }
     }, // a function which handles a get request for all messages
     post: function (req, res) {
       if (!req.body) {
         return res.status(400).send('Invalid POST Request');
       } else {
-        console.log(req.body);
-        console.log(req.body.message,req.body.username,req.body.roomname)
         var result = models.messages.post(req.body.message,req.body.username,req.body.roomname);
         result.then(val=>{
           return res.status(201).send('Message Added');
         }).catch(err=>{
-          console.log(err)
           return res.status(500).send('Unable to Add Message');
         });
       }
@@ -32,8 +28,29 @@ module.exports = {
 
   users: {
     // Ditto as above
-    get: function (req, res) {},
-    post: function (req, res) {}
+    get: function (req, res) {
+      if (!req.body) {
+        return res.status(400).send('Invalid GET Request');
+      } else {
+        models.users.get().then(users => {
+        var response = {results: users};
+        return res.status(200).send(response);
+      });
+    }
+    },
+    post: function (req, res) {
+      if (!req.body) {
+        return res.status(400).send('Invalid POST Request');
+      } else {
+        var result = models.users.post(req.body.username);
+        result.then(val=>{
+          return res.status(201).send('User Added');
+        }).catch(err=>{
+          console.log(err);
+          return res.status(500).send('Unable to Add User');
+        });
+      }
+    }
   }
 };
 
--- server/schema.sql
+++ server/schema.sql
@@ -1,4 +1,6 @@
 -- CREATE DATABASE chat;
+DROP DATABASE chat;
+CREATE DATABASE chat;
 
 USE chat;
 
@@ -12,18 +14,20 @@ CREATE TABLE rooms (
 CREATE TABLE users (
   /* Describe your table here.*/
   /* id, name*/
-  id int, name text, PRIMARY KEY (id)
+  id int not null auto_increment, name text, PRIMARY KEY (id)
 );
 
 CREATE TABLE messages (
   /* Describe your table here.*/
   /*id, time stamp, text, should point to a user id and a room id*/
-  id int not null auto_increment, time bigint, text text, user text, room text, PRIMARY KEY (id) 
+  id int not null auto_increment, createdAt bigint, message text, user text, room text, PRIMARY KEY (id) 
 );
 /* Create other tables and define schemas for them here! */
 
 
 alter table messages auto_increment=1001;
+
+alter table users auto_increment=1001;
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
